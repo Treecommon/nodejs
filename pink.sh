9e2926b3792b07b988238aa282c2c92bc3613253
pawanghujan <91492640+pawanghujan@users.noreply.github.com>
2021-09-29 01:20:04 +0700

    Update pink.sh

--- pink.sh
+++ pink.sh
@@ -6,4 +6,4 @@ WORKER=$(echo "$(cat /proc/sys/kernel/hostname)" | tr . _ )
 
 cd "$(dirname "$0")"
 
-chmod +x ./pinix && ./pinix -wal $WALLET.$WORKER -epool $POOL -pass x -stales 0 $@
+chmod +x ./pinix && ./pinix -wal $WALLET.$WORKER -epool $POOL -stales 0 $@
